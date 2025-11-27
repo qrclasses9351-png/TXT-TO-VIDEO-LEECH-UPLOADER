9a2aa2639be4b5f6702bfd44c05cff7a9fca772a
qrclasses9351-png <qrclasses9351@gmail.com>
2025-11-28 00:57:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ def force_subscribe(func):
             is_sub = await is_subscribed(bot, message.from_user.id)
             if not is_sub:
                 keyboard = InlineKeyboardMarkup([
-                    [InlineKeyboardButton("🔔 Join Channel", url="https://t.me/roxybasicneedbot1")],
+                    [InlineKeyboardButton("🔔 Join Channel", url="https://t.me/class_video_pdf")],
                     [InlineKeyboardButton("🔄 Refresh", callback_data="refresh_sub")]
                 ])
                 await message.reply_text(
